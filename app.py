dac8ae8140421d4a8050e7c39cb5c14cb82a63d5
kishorravi <ece.kishor@gmail.com>
2023-12-21 14:10:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,44 @@
-from flask import Flask, render_template, Response
+# app.py
+from flask import Flask, render_template, request, jsonify
 import cv2
+import numpy as np
+import base64
 
 app = Flask(__name__)
 
-# OpenCV video capture
-cap = cv2.VideoCapture(0)
-
-def generate_frames():
-    while True:
-        success, frame = cap.read()
-        if not success:
-            break
-        else:
-            # Perform face detection here (using a pre-trained face detection model)
-            # You can use Haar cascades or a deep learning-based face detection model
-            # For simplicity, we'll just display the webcam feed without face detection
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+# Load the Haar Cascade for face detection
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/detect', methods=['POST'])
+def detect():
+    # Get the image data from the request
+    img_data = request.form['image']
+    
+    # Convert base64 image data to NumPy array
+    img_array = np.frombuffer(base64.b64decode(img_data), dtype=np.uint8)
+    
+    # Decode the image
+    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    
+    # Convert the image to grayscale for face detection
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Detect faces in the image
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    
+    # Draw rectangles around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    
+    # Encode the image to base64
+    _, buffer = cv2.imencode('.jpg', img)
+    img_str = base64.b64encode(buffer).decode('utf-8')
+    
+    return jsonify({'image': img_str})
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
