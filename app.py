c8d7c112e605f35f67cdaa612483f1d3237cada5
kishorravi <ece.kishor@gmail.com>
2023-12-21 14:31:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask, render_template, request, jsonify
 import cv2
 import numpy as np
@@ -6,38 +5,30 @@ import base64
 
 app = Flask(__name__)
 
-# Load the Haar Cascade for face detection
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/detect', methods=['POST'])
-def detect():
-    # Get the image data from the request
+@app.route('/add_furniture', methods=['POST'])
+def add_furniture():
+    # Get the image data and furniture position from the request
     img_data = request.form['image']
-    
+    furniture_x = int(request.form['furnitureX'])
+    furniture_y = int(request.form['furnitureY'])
+
     # Convert base64 image data to NumPy array
     img_array = np.frombuffer(base64.b64decode(img_data), dtype=np.uint8)
-    
+
     # Decode the image
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    
-    # Convert the image to grayscale for face detection
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    # Detect faces in the image
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-    
-    # Draw rectangles around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    
-    # Encode the image to base64
+
+    # Add a simple rectangle representing a sofa to the image
+    cv2.rectangle(img, (furniture_x, furniture_y), (furniture_x + 100, furniture_y + 50), (0, 0, 255), 2)
+
+    # Encode the modified image to base64
     _, buffer = cv2.imencode('.jpg', img)
     img_str = base64.b64encode(buffer).decode('utf-8')
-    
+
     return jsonify({'image': img_str})
 
 if __name__ == '__main__':
